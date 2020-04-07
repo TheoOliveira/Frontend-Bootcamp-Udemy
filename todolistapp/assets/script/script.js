d80fe48758caff81f9bb28cec78263839c308af7
TheoOliveira <theophilo.oliveira@gmail.com>
2020-04-06 21:56:07 -0300

    nao sei o que ta diferente acho que so bootcamp

---
+++ todolistapp/assets/script/script.js
@@ -0,0 +1,38 @@
+//Variaveis
+
+let completeBtn = document.querySelectorAll('li');
+let deleteBtn = document.querySelectorAll('span');
+let input = document.querySelector(`input[type='text']`)
+let ul = document.querySelector('ul');
+
+//Completa  a tarefa ao clicar
+
+for (let el of completeBtn) {
+    el.addEventListener('click', () => {
+        el.classList.toggle('completed');
+    })
+}
+
+//  Deleta o item
+
+for(let el of deleteBtn){
+    el.addEventListener('click', (event)=>{
+        el.parentNode.remove(); //criar animação no js de fadeout com função
+        event.stopPropagation();
+    })
+}
+
+// Adiciona input com enter
+
+input.addEventListener('keypress',(e)=>{
+    if(e.which === 13){
+        let text = input.value;
+        input.value = '';
+        let li = document.createElement('li');
+        let span = document.createElement('span');
+        li.appendChild(span);
+        ul.appendChild(li);
+        li.append(`${text}`);
+        span.append('X ');
+    }
+})
