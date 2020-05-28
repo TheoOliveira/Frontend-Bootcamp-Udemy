01d56150978b7727797d96c3aac179cd2b32462a
TheoOliveira <theophilo.oliveira@gmail.com>
2020-05-27 22:38:01 -0300

    parado no js do todo

--- todolistapp/assets/script/script.js
+++ todolistapp/assets/script/script.js
@@ -1,32 +1,32 @@
 //Variaveis
 
-let li = document.getElementById ('li');
+let li = document.getElementById('li');
 let input = document.querySelector(`input[type='text']`)
 let ul = document.getElementById('ul');
+document.querySelector('ul').addEventListener('click', completeItem);
 
 //Completa  a tarefa ao clicar
 
-    // li.addEventListener('click', (e) => {
-    //     let val = e.tTagNamearget;
-    //     if(val.tagName.toUpperCase() == 'LI'){
-    //       val.classList.toggle('completed');
-    //     }
-    //     //e.classList.toggle('completed');
-    //
-    // })
+// li.addEventListener('click', (e) => {
+//     let val = e.tTagNamearget;
+//     if(val.tagName.toUpperCase() == 'LI'){
+//       val.classList.toggle('completed');
+//     }
+//     //e.classList.toggle('completed');
+//
+// })
 
 let val = 'incompleto';
 
 // Complete item and delete
-function completeItem(){
-  ul.addEventListener("click",(e)=> {
+function completeItem() {
+  ul.addEventListener("click", (e) => {
     let tgt = e.target;
-    if (tgt.tagName.toUpperCase() == "LI" && val == 'incompleto') {
-      tgt.classList.toggle('completed');
-      val = 'completo';
-    }else {
-      removeAnimation(tgt);
-      val= 'incompleto';
+    if (tgt.tagName.toUpperCase() == "LI" &&  tgt.className == 'incompleto') {
+      tgt.className.add('completo');
+      tgt.className.remove('incompleto');
+    } if(tgt.tagName.toUpperCase() == "LI" && tgt.className == 'completo') {
+      removeItem(tgt);      
     }
 
   });
@@ -34,25 +34,22 @@ function completeItem(){
 }
 // tentativa de criar efeito de fadeOut
 function removeItem(element) {
-    element.remove();
-}
-function removeAnimation(element) {
-  element.classList.add('fade-out');
-  
-  // setInterval(removeItem(element));
+  element.remove(element);
 }
 
 
 
-input.addEventListener('keypress',(e)=>{
-    console.log(e);
-    if(e.key === "Enter"){
-        let text = input.value;
-        input.value = '';
-        let li = document.createElement('li');
-        ul.appendChild(li);
-        li.append(`${text}`);
-    }
+
+input.addEventListener('keypress', (e) => {
+  console.log(e);
+  if (e.key === "Enter") {
+    let text = input.value;
+    input.value = '';
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.className = 'incompleto';  
+    li.append(`${text}`);
+  }
 })
 
 completeItem();
